6606076201728a981d3d4f43e6849e559e06fbba
habil <habiledgar111@gmail.com>
2022-05-29 19:42:40 +0700

    membuat halaman login

--- app/src/main/java/com/example/tugas_akhir_pam/login.java
+++ app/src/main/java/com/example/tugas_akhir_pam/login.java
@@ -3,6 +3,7 @@ package com.example.tugas_akhir_pam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void sign_in(View view) {
     }
 }
