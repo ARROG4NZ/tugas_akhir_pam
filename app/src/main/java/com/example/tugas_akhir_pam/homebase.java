b03d8d30ca379465444cc50b863638c8b225f018
habil <habiledgar111@gmail.com>
2022-05-29 19:44:50 +0700

    membuat halaman homebase

---
+++ app/src/main/java/com/example/tugas_akhir_pam/homebase.java
@@ -0,0 +1,14 @@
+package com.example.tugas_akhir_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homebase);
+    }
+}
