148fdeb20d9489480ff914ce37317799b4efdb0c
habil <habiledgar111@gmail.com>
2022-06-01 20:30:32 +0700

    menghubungkan ke firebase

--- app/src/main/java/com/example/tugas_akhir_pam/signup.java
+++ app/src/main/java/com/example/tugas_akhir_pam/signup.java
@@ -3,6 +3,7 @@ package com.example.tugas_akhir_pam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class signup extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void sign_up(View view) {
     }
 }
