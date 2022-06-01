81d1faab146e9200791d52e87e7aecde6e9db736
habil <habiledgar111@gmail.com>
2022-06-01 21:38:50 +0700

    build berhasil tapi masih belum dilakukan pengujian

--- app/src/main/java/com/example/tugas_akhir_pam/login.java
+++ app/src/main/java/com/example/tugas_akhir_pam/login.java
@@ -1,18 +1,106 @@
 package com.example.tugas_akhir_pam;
-
+import static android.content.ContentValues.TAG;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class login extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.emailAdress);
+        password = findViewById(R.id.password);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //melakukan pengecekan apakah sebelumnya sudah melakukan login
+        FirebaseUser user = mAuth.getCurrentUser();
+        updateUI(user);
     }
 
     public void sign_in(View view) {
+        //melakukan pengecekan apakah form kosong
+        if(!validateForm()){
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //login berhasil
+                    Log.d(TAG,"login berhasil");
+                    //setelah berhasil maka akan dilakukan penyimpanan user pada penyimpanan sementara
+                    FirebaseUser usernow =mAuth.getCurrentUser();
+                    Toast.makeText(login.this, usernow.toString(), Toast.LENGTH_SHORT).show();
+                    updateUI(usernow);
+                } else {
+                    Log.w(TAG, "login gagal\n",task.getException());
+                    Toast.makeText(login.this,"login gagal",Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+
+                }
+            }
+        });
     }
+
+    //mengarahkan ke halaman sign_up
+    public void to_sign_up(View view) {
+    Intent inten = new Intent(this, signup.class);
+    startActivity(inten);
+    }
+
+    //perpindahan ke halaman utama
+    public void updateUI(FirebaseUser user){
+        if(user != null){
+            Intent inten = new Intent(this,MainActivity.class);
+            startActivity(inten);
+        }else {
+            Toast.makeText(login.this,"Log In First",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //mengecek apakah form terisi
+    public boolean validateForm(){
+        boolean result = true;
+        if(TextUtils.isEmpty(email.getText().toString())){
+            email.setError("Required");
+            result = false;
+        }else{
+            email.setError(null);
+        }
+
+        if(TextUtils.isEmpty(password.getText().toString())){
+            password.setError("Required");
+            result = false;
+        }else{
+            password.setError(null);
+        }
+
+        return result;
+    }
+
+
 }
--- app/src/main/java/com/example/tugas_akhir_pam/signup.java
+++ app/src/main/java/com/example/tugas_akhir_pam/signup.java
@@ -1,18 +1,92 @@
 package com.example.tugas_akhir_pam;
 
+import static android.content.ContentValues.TAG;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class signup extends AppCompatActivity {
-
+    private FirebaseAuth mAUth;
+    private FirebaseUser user;
+    private EditText username,email,password,repeat_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mAUth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.sign_up_username);
+        email = findViewById(R.id.sign_up_email);
+        password = findViewById(R.id.sign_up_password);
+        repeat_password = findViewById(R.id.sign_up_repeat_password);
     }
 
     public void sign_up(View view) {
+        mAUth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "membuat user dengan email dan password berhasil");
+                    user = mAUth.getCurrentUser();
+                    Toast.makeText(signup.this, user.toString(), Toast.LENGTH_SHORT).show();
+                    updateUI(user);
+                }else{
+                    Log.w(TAG, "gagal membuat user");
+                    Toast.makeText(signup.this, task.getException().toString(),Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    //mengecek apakah form terisi
+    public boolean validateForm(){
+        boolean result = true;
+        if(TextUtils.isEmpty(username.getText().toString())){
+            username.setError("Required");
+            result = false;
+        }
+        if(TextUtils.isEmpty(email.getText().toString())){
+            email.setError("Required");
+            result = false;
+        }
+        if(TextUtils.isEmpty(password.getText().toString())){
+            password.setError("Required");
+            result = false;
+        }
+        if (TextUtils.isEmpty(repeat_password.getText().toString())){
+            repeat_password.setError("Required");
+            result = false;
+        }else{
+            if(!repeat_password.getText().toString().equals(password.getText().toString())){
+                repeat_password.setError("Password berbeda");
+                result = false;
+            }
+        }
+
+        return result;
+    }
+
+    //mengubah tampilan
+    public void updateUI(FirebaseUser userUI){
+        if(userUI != null){
+            Intent inten = new Intent(signup.this,login.class);
+            startActivity(inten);
+        } else {
+            Toast.makeText(signup.this,"Buat akun terlebih dahulu",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
